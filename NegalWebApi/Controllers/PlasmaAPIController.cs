928be9efcce88904e60d2d1c48113547330c5c73
artur.moshkola <artur.moshkola@aska.ua>
2021-04-12 13:53:50 +0300

    Add project files.

---
+++ NegalWebApi/Controllers/PlasmaAPIController.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NegalWebApi.Controllers
+{
+	[ApiController]
+	[Route("[controller]")]
+	public class PlasmaAPIController : ControllerBase
+	{
+		[HttpPost]
+		public IActionResult GetServerTime()
+		{
+			throw new NotImplementedException();
+		}
+
+		[HttpPost]
+		public IActionResult GetOperatorInfo()
+		{
+			throw new NotImplementedException();
+		}
+
+		[HttpPost]
+		public IActionResult GetOperatorPhoto()
+		{
+			throw new NotImplementedException();
+		}
+
+		[HttpPost]
+		public IActionResult GetDonorsInfo()
+		{
+			throw new NotImplementedException();
+		}
+
+		[HttpPost]
+		public IActionResult GetDonorsPhoto()
+		{
+			throw new NotImplementedException();
+		}
+
+		[HttpPost]
+		public IActionResult GetEquipments()
+		{
+			throw new NotImplementedException();
+		}
+
+		[HttpPost]
+		public IActionResult GetSodiumChlorides()
+		{
+			throw new NotImplementedException();
+		}
+
+		[HttpPost]
+		public IActionResult GetAnticoagulants()
+		{
+			throw new NotImplementedException();
+		}
+
+		[HttpPost]
+		public IActionResult SavePlasmaCollection()
+		{
+			throw new NotImplementedException();
+		}
+
+		[HttpPost]
+		public IActionResult SaveException()
+		{
+			throw new NotImplementedException();
+		}
+
+		[HttpPost]
+		public IActionResult GetPlasmaParmas()
+		{
+			throw new NotImplementedException();
+		}
+
+		[HttpPost]
+		public IActionResult UploadPlasmaParams()
+		{
+			throw new NotImplementedException();
+		}
+
+		[HttpPost]
+		public IActionResult UploadPlasmaComment()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
