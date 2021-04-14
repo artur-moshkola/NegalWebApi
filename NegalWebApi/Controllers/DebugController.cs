05eeb231f087bbc4600faf1b0464766e0d466c72
Artur Moshkola <artur.moshkola@gmail.com>
2021-04-14 21:13:56 +0300

    Logs and any prefix

--- NegalWebApi/Controllers/DebugController.cs
+++ NegalWebApi/Controllers/DebugController.cs
@@ -13,39 +13,60 @@ namespace NegalWebApi.Controllers
 	[ApiController]
 	public class DebugController : ControllerBase
 	{
-		private async ValueTask<IActionResult> Debug(string method, Func<string, string> transform = null)
+		private readonly ILogger logger;
+
+		public DebugController(ILogger<DebugController> logger)
 		{
+			this.logger = logger;
+		}
+
+		private async ValueTask<IActionResult> Debug(string url, Func<string, string> transform = null)
+		{
+			var method = String.Empty;
 			try
 			{
+				var dtid = String.Empty;
+				var up = url.Split('/');
+				if (String.Equals("plasmaApi", up[^2], StringComparison.InvariantCultureIgnoreCase)) dtid = up[^1];
+				logger.LogInformation($"Called method: {url}.");
+				method = String.Join('-', up);
 				using (var fl1 = System.IO.File.OpenWrite($"Logs/{DateTime.Now:yyyy-MM-dd_HH-mm-ss-fff}_{method}_Request.json"))
 				{
 					await Request.BodyReader.CopyToAsync(fl1);
 				}
-				string resp;
-				var rname = $"Data/{method}.json";
-				if (!System.IO.File.Exists(rname)) rname = $"Data/Default.json";
-				using (var fd = System.IO.File.OpenRead(rname))
+				string resp = null;
+				if (!String.IsNullOrEmpty(dtid))
 				{
-					using var sr = new StreamReader(fd);
-					resp = await sr.ReadToEndAsync();
+					var rname = $"Data/{dtid}.json";
+					if (!System.IO.File.Exists(rname)) rname = $"Data/Default.json";
+					using (var fd = System.IO.File.OpenRead(rname))
+					{
+						using var sr = new StreamReader(fd);
+						resp = await sr.ReadToEndAsync();
+					}
+
+					if (transform != null)
+						resp = transform(resp);
 				}
 
-				if (transform != null)
-					resp = transform(resp);
-
-				using (var fl2 = System.IO.File.OpenWrite($"Logs/{DateTime.Now:yyyy-MM-dd_HH-mm-ss-fff}_{method}_Response.json"))
+				if (resp != null)
 				{
-					using var sw = new StreamWriter(fl2);
-					await sw.WriteAsync(resp);
+					using (var fl2 = System.IO.File.OpenWrite($"Logs/{DateTime.Now:yyyy-MM-dd_HH-mm-ss-fff}_{method}_Response.json"))
+					{
+						using var sw = new StreamWriter(fl2);
+						await sw.WriteAsync(resp);
+					}
+					return Content(resp, "application/json");
 				}
 
-				return Content(resp, "application/json");
+				throw new Exception("Bad api path");
 			}
 			catch (Exception e)
 			{
 				using var fle = System.IO.File.OpenWrite($"Logs/{DateTime.Now:yyyy-MM-dd_HH-mm-ss-fff}_{method}_Exception.log");
 				using var sw = new StreamWriter(fle);
 				await sw.WriteAsync(e.ToString());
+				logger.LogError(e, "Unhandled Exception");
 				throw;
 			}
 		}
@@ -56,7 +77,7 @@ namespace NegalWebApi.Controllers
 		[Route("{*url}")]
 		public ValueTask<IActionResult> Wildcard(string url)
 		{
-			return Debug(url.Replace('/','-'), s => rg.Replace(s, new MatchEvaluator(m => String.Format(m.Groups[1].Value, DateTime.Now))));
+			return Debug(url, s => rg.Replace(s, new MatchEvaluator(m => String.Format(m.Groups[1].Value, DateTime.Now))));
 		}
 	}
 }
--- NegalWebApi/Startup.cs
+++ NegalWebApi/Startup.cs
@@ -26,6 +26,7 @@ namespace NegalWebApi
 		{
 
 			services.AddControllers();
+			services.AddLogging(logging => logging.AddConsole());
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
