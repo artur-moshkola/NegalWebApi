2230c1b0eff469c36b4c7727f63ec491d958f984
artur.moshkola <artur.moshkola@aska.ua>
2021-04-15 22:46:08 +0300

    Proxy mode w

--- NegalWebApi/Controllers/DebugController.cs
+++ NegalWebApi/Controllers/DebugController.cs
@@ -8,7 +8,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Net.Http;
-using System.Net.Http.Headers;
+using Microsoft.Extensions.Configuration;
 
 namespace NegalWebApi.Controllers
 {
@@ -17,14 +17,19 @@ namespace NegalWebApi.Controllers
 	{
 		private readonly ILogger logger;
 		private readonly HttpClient http;
+		private readonly IConfiguration conf;
 
-		public DebugController(HttpClient http, ILogger<DebugController> logger)
+		public DebugController(IConfiguration conf, HttpClient http, ILogger<DebugController> logger)
 		{
+			this.conf = conf;
 			this.http = http;
 			this.logger = logger;
 		}
 
-		private async ValueTask<IActionResult> Debug(string url, Func<string, string> transform = null)
+		private const string ContentTypeHeader = "Content-Type";
+		private const string JsonContentType = "application/json";
+
+		private async ValueTask Debug(string url, Func<string, string> transform = null)
 		{
 			var method = String.Empty;
 			try
@@ -38,7 +43,7 @@ namespace NegalWebApi.Controllers
 				{
 					using (var fl1 = System.IO.File.OpenWrite($"Dumps/{DateTime.Now:yyyy-MM-dd_HH-mm-ss-fff}_{method}_Request.json"))
 					{
-						await Request.BodyReader.CopyToAsync(fl1);
+						await Request.Body.CopyToAsync(fl1).ConfigureAwait(false);
 					}
 					string resp;
 					var rname = $"Data/{dtid}.json";
@@ -46,7 +51,7 @@ namespace NegalWebApi.Controllers
 					using (var fd = System.IO.File.OpenRead(rname))
 					{
 						using var sr = new StreamReader(fd);
-						resp = await sr.ReadToEndAsync();
+						resp = await sr.ReadToEndAsync().ConfigureAwait(false);
 					}
 
 					if (transform != null)
@@ -55,52 +60,84 @@ namespace NegalWebApi.Controllers
 					using (var fl2 = System.IO.File.OpenWrite($"Dumps/{DateTime.Now:yyyy-MM-dd_HH-mm-ss-fff}_{method}_Response.json"))
 					{
 						using var sw = new StreamWriter(fl2);
-						await sw.WriteAsync(resp);
-					}
-					return Content(resp, "application/json");
-				}
-
-				using var msrq = new MemoryStream();
-				await Request.BodyReader.CopyToAsync(msrq);
-				using (var fl1 = System.IO.File.OpenWrite($"Logs/{DateTime.Now:yyyy-MM-dd_HH-mm-ss-fff}_{method}_Request.json"))
-				{
-					await msrq.CopyToAsync(fl1);
-				}
-				
-				using var cnt = new StreamContent(msrq);
-				cnt.Headers.ContentType = new MediaTypeHeaderValue("application/json");
-				using var presp = await http.PostAsync("http://localhost:9191/" + url, cnt);
-				if (presp.IsSuccessStatusCode)
-				{
-					string rsp;
-					using (var msrs = new MemoryStream()) {
-						await presp.Content.CopyToAsync(msrs);
-						rsp = Encoding.UTF8.GetString(msrs.GetBuffer());
+						await sw.WriteAsync(resp).ConfigureAwait(false);
 					}
 
-					using (var fl2 = System.IO.File.OpenWrite($"Logs/{DateTime.Now:yyyy-MM-dd_HH-mm-ss-fff}_{method}_Response.json"))
-					{
-						using var sw = new StreamWriter(fl2);
-						await sw.WriteAsync(rsp);
-					}
-					return Content(rsp, "application/json");
+					Response.StatusCode = 200;
+					Response.Headers.Add(ContentTypeHeader, JsonContentType);
+					await Response.BodyWriter.WriteAsync(Encoding.UTF8.GetBytes(resp).AsMemory()).ConfigureAwait(false);
 				}
 				else
 				{
-					var rsc = (int)presp.StatusCode;
-					using (var fl2 = System.IO.File.OpenWrite($"Logs/{DateTime.Now:yyyy-MM-dd_HH-mm-ss-fff}_{method}_Response.json"))
+					using (var fl2 = System.IO.File.OpenWrite($"Dumps/{DateTime.Now:yyyy-MM-dd_HH-mm-ss-fff}_{method}_Response_404.json"))
 					{
-						using var sw = new StreamWriter(fl2);
-						await sw.WriteAsync($"Status code: {rsc}");
+						
 					}
-					return StatusCode(rsc);
+					Response.StatusCode = 404;
 				}
 			}
 			catch (Exception e)
 			{
 				using var fle = System.IO.File.OpenWrite($"Logs/{DateTime.Now:yyyy-MM-dd_HH-mm-ss-fff}_{method}_Exception.log");
 				using var sw = new StreamWriter(fle);
-				await sw.WriteAsync(e.ToString());
+				await sw.WriteAsync(e.ToString()).ConfigureAwait(false);
+				logger.LogError(e, "Unhandled Exception");
+				throw;
+			}
+		}
+
+		private async ValueTask Proxy(string url)
+		{
+			var method = String.Empty;
+			try
+			{
+				logger.LogInformation($"Called method: {url}.");
+				method = url.Replace('/', '-');
+				int rcode;
+				string[] rscnts;
+				using (var rsms = new MemoryStream())
+				{
+					using (var rqms = new MemoryStream())
+					{
+						await Request.BodyReader.CopyToAsync(rqms).ConfigureAwait(false);
+						rqms.Seek(0, SeekOrigin.Begin);
+						using (var fl1 = System.IO.File.OpenWrite($"Logs/{DateTime.Now:yyyy-MM-dd_HH-mm-ss-fff}_{method}_Request.json"))
+						{
+							await rqms.CopyToAsync(fl1).ConfigureAwait(false);
+						}
+
+						rqms.Seek(0, SeekOrigin.Begin);
+						using var cnt = new StreamContent(rqms);
+						IEnumerable<string> cntt = Request.Headers[ContentTypeHeader];
+						cnt.Headers.Add(ContentTypeHeader, cntt);
+
+						var ub = new UriBuilder(conf.GetValue<string>("ProxyTarget"))
+						{
+							Path = url
+						};
+						using var presp = await http.PostAsync(ub.Uri, cnt).ConfigureAwait(false);
+						rcode = (int)presp.StatusCode;
+						rscnts = presp.Headers.Where(h => h.Key == ContentTypeHeader).Select(h => h.Value).FirstOrDefault()?.ToArray();
+						await presp.Content.CopyToAsync(rsms).ConfigureAwait(false);
+						rsms.Seek(0, SeekOrigin.Begin);
+					}
+
+					using (var fl2 = System.IO.File.OpenWrite($"Logs/{DateTime.Now:yyyy-MM-dd_HH-mm-ss-fff}_{method}_Response_{rcode}.json"))
+					{
+						await rsms.CopyToAsync(fl2).ConfigureAwait(false);
+					}
+					rsms.Seek(0, SeekOrigin.Begin);
+
+					Response.StatusCode = rcode;
+					Response.Headers.Add(ContentTypeHeader, rscnts);
+					await Response.BodyWriter.WriteAsync(rsms.ToArray().AsMemory()).ConfigureAwait(false);
+				}
+			}
+			catch (Exception e)
+			{
+				using var fle = System.IO.File.OpenWrite($"Logs/{DateTime.Now:yyyy-MM-dd_HH-mm-ss-fff}_{method}_Exception.log");
+				using var sw = new StreamWriter(fle);
+				await sw.WriteAsync(e.ToString()).ConfigureAwait(false);
 				logger.LogError(e, "Unhandled Exception");
 				throw;
 			}
@@ -110,9 +147,14 @@ namespace NegalWebApi.Controllers
 
 		[HttpPost]
 		[Route("{*url}")]
-		public ValueTask<IActionResult> Wildcard(string url)
+		public ValueTask Wildcard(string url)
 		{
-			return Debug(url, s => rg.Replace(s, new MatchEvaluator(m => String.Format(m.Groups[1].Value, DateTime.Now))));
+			return conf.GetValue<string>("Mode") switch
+			{
+				nameof(Debug) => Debug(url, s => rg.Replace(s, new MatchEvaluator(m => String.Format(m.Groups[1].Value, DateTime.Now)))),
+				nameof(Proxy) => Proxy(url),
+				_ => throw new Exception("Mode is not set"),
+			};
 		}
 	}
 }
