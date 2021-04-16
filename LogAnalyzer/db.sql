32ccec42dbb6e88547b924f397180a2eff7e3abb
artur.moshkola <artur.moshkola@aska.ua>
2021-04-16 23:09:40 +0300

    Log Analysis

---
+++ LogAnalyzer/db.sql
@@ -0,0 +1,55 @@
+﻿/*
+drop table if exists RawLogs;
+*/
+
+if not exists(select * from INFORMATION_SCHEMA.TABLES where TABLE_SCHEMA=N'dbo' and TABLE_NAME=N'RawLogs')
+create table RawLogs (
+	[Package] nvarchar(64) not null,
+	[Name] nvarchar(380) not null,
+		constraint PK_RawLogs primary key ([Package], [Name]),
+	[RequestId] nvarchar(24) not null,
+	[TS] datetime2,
+	[Method] nvarchar(1024),
+	[MessageType] nvarchar(16),
+	HttpStatusCode int,
+	ProcTime int,
+	[Data] nvarchar(max),
+	[DataType] nvarchar(16)
+)
+go
+
+create or alter procedure ImportFile
+	@Package nvarchar(64),
+	@Name nvarchar(380),
+	@RequestId nvarchar(24),
+	@TS datetime2 = null,
+	@Method nvarchar(1024) = null,
+	@MessageType nvarchar(16) = null,
+	@HttpStatusCode int = null,
+	@ProcTime int = null,
+	@Data nvarchar(max) = null,
+	@DataType nvarchar(16) = null
+as
+begin
+	set nocount on;
+
+	insert into RawLogs ([Package], [Name], [RequestId], [TS], [Method], MessageType, HttpStatusCode, ProcTime, [Data], [DataType])
+	values (@Package, @Name, @RequestId, @TS, @Method, @MessageType, @HttpStatusCode, @ProcTime, @Data, @DataType);
+
+end;
+go
+
+create or alter view Requests as
+select rq.[Package], rq.[RequestId], rq.[Method],
+	[Request]=rq.[Data],
+	[ResponseCode]=rs.HttpStatusCode,
+	[Response]=rs.[Data],
+	[Exception]=ex.[Data],
+	ProcTime=coalesce(rs.ProcTime, ex.ProcTime),
+	[DeviceId]=json_value(rq.[Data], '$.gooidKey')
+from RawLogs rq
+left join RawLogs rs on rs.[Package]=rq.[Package] and rs.[RequestId]=rq.[RequestId] and rs.Method=rq.Method and rs.[MessageType]=N'Response'
+left join RawLogs ex on ex.[Package]=rq.[Package] and ex.[RequestId]=rq.[RequestId] and ex.Method=rq.Method and ex.[MessageType]=N'Exception'
+where rq.MessageType=N'Request';
+
+go
---
+++ LogAnalyzer/dbdump.ps1
@@ -0,0 +1,69 @@
+param (
+	[string]$package = $null,
+    [string]$path = 'Logs',
+    [string]$cs
+)
+
+try {
+
+    if ([System.String]::IsNullOrEmpty($package)) {
+        $package = Get-Date -Format "yyyy-MM-dd-HH-mm-ss"
+    }
+
+    $scon = New-Object System.Data.SqlClient.SqlConnection
+    $scon.ConnectionString = $cs
+    $scon.Open()
+
+    Get-ChildItem -File -Path $path | ForEach-Object {
+        $c = Get-Content -Path $_.FullName -Encoding "utf8"
+        try {
+            $params = @()
+            $params += New-Object System.Data.SqlClient.SqlParameter("@Package",$package)
+            $params += New-Object System.Data.SqlClient.SqlParameter("@Name",$_.Name)
+
+            if ($_.Name -match '^([0-9]{4}-[0-9]{2}-[0-9]{2}_[0-9]{2}-[0-9]{2}-[0-9]{2}-[0-9]{3,8})_([^_]*)_([^\._]*)(?:_([0-9]+))?(?:_([0-9]+)ms)?\.(.*)$') {
+                $params += New-Object System.Data.SqlClient.SqlParameter("@RequestId", $Matches[1])
+                $ts = [System.DateTime]::ParseExact($Matches[1], "yyyy-MM-dd_HH-mm-ss-fff", [System.Globalization.CultureInfo]::InvariantCulture)
+                $params += New-Object System.Data.SqlClient.SqlParameter("@TS", $ts)
+                $params += New-Object System.Data.SqlClient.SqlParameter("@Method", $Matches[2].Replace('-','/'))
+                $params += New-Object System.Data.SqlClient.SqlParameter("@MessageType", $Matches[3])
+                if (![System.String]::IsNullOrEmpty($Matches[4])) {
+                    $params += New-Object System.Data.SqlClient.SqlParameter("@HttpStatusCode", [System.Int32]::Parse($Matches[4]))
+                }
+                if (![System.String]::IsNullOrEmpty($Matches[5])) {
+                    $params += New-Object System.Data.SqlClient.SqlParameter("@ProcTime", [System.Int32]::Parse($Matches[5]))
+                }
+                $params += New-Object System.Data.SqlClient.SqlParameter("@DataType", $Matches[6])
+            }
+
+            $params += New-Object System.Data.SqlClient.SqlParameter("@Data",[string]$c)
+
+            
+            $cmd = New-Object System.Data.SqlClient.SqlCommand
+            $cmd.Connection = $scon
+            $cmd.CommandTimeout = 0
+            $cmd.CommandType = [System.Data.CommandType]::StoredProcedure
+            $cmd.CommandText = "dbo.ImportFile"
+
+            ForEach ($p in $params) {
+                $cmd.Parameters.Add($p) | Out-Null
+            }
+
+            $cmd.ExecuteNonQuery() | Out-Null
+        }
+        catch [Exception] {
+            Write-Error $_.Exception.Message
+        }
+        finally {
+            $cmd.Dispose()
+        }
+
+        Write-Host $_.Name
+    }
+}
+catch [Exception] {
+    Write-Error $_.Exception.Message
+}
+finally {
+    $scon.Dispose()
+}
